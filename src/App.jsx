437f6f2792d7c652ca47cb12b46d3a7eb1d6da17
ATHARVA279 <141947324+ATHARVA279@users.noreply.github.com>
2024-01-06 15:19:54 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -23,8 +23,33 @@ const imageData = ()=>{
   return data;
 }
 
+let data = imageData()
+
 function App() {
   // code here
+
+  let headingStyle = {
+    textAlign:"center"
+  }
+
+  let styleImg = {
+    display:"grid",
+    gridTemplateColumns:"repeat(2,1fr)",
+    alignSelf:"center"
+  }
+
+  return(
+    <>
+    <h1 style={headingStyle}>Kalvium Gallery</h1>
+
+    <div style={styleImg}>
+      <img src={data[0].img} alt="" width={"550px"} style={{margin:"auto", marginBottom:"50px"}}/>
+      <img src={data[1].img} alt="" width={"550px"} style={{margin:"auto",marginBottom:"50px"}}/>
+      <img src={data[2].img} alt="" width={"550px"} style={{margin:"auto",marginBottom:"50px"}}/>
+      <img src={data[3].img} alt="" width={"550px"} style={{margin:"auto",marginBottom:"50px"}}/>
+    </div>
+    </>
+  )
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -26,5 +26,31 @@ export default class AppClass extends Component{
     return data;
   }
 
+  render(){
+    this.data = this.imageData()
+
+    let headingStyle = {
+      textAlign:"center"
+    }
+  
+    let styleImg = {
+      display:"grid",
+      gridTemplateColumns:"repeat(2,1fr)",
+      alignSelf:"center"
+    }
+
+    return(
+      <>
+      <h1 style={headingStyle}>Kalvium Gallery</h1>
+      <div style={styleImg}>
+      <img src={this.data[0].img} alt="" width={"550px"} style={{margin:"auto", marginBottom:"50px"}}/>
+      <img src={this.data[1].img} alt="" width={"550px"} style={{margin:"auto",marginBottom:"50px"}}/>
+      <img src={this.data[2].img} alt="" width={"550px"} style={{margin:"auto",marginBottom:"50px"}}/>
+      <img src={this.data[3].img} alt="" width={"550px"} style={{margin:"auto",marginBottom:"50px"}}/>
+    </div>
+      </>
+    )
+  }
+
   // code here
 }
